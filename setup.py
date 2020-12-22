30d9cac11d9a77abb4fc59a0aa86bf668d5e71d8
BradleyGrantham <bgrantham343@gmail.com>
2020-12-22 11:16:04 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="jeebee",
+    version="0.0.1",
+    description="A GameBattles Discord bot.",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8.0",
+)
